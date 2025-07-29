8913e58eea92a9c9040766588dfe2f632f2143f7
vindennt <truong.dennis00@gmail.com>
2025-07-28 19:50:57 -0700

    Disable verbose logger

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .config import settings
-from starlette.middleware.base import BaseHTTPMiddleware
-from .middleware.logger import mw_logger
+# from .middleware.logger import mw_logger
 from .controllers import health
 
 app = FastAPI()
@@ -16,9 +15,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-# app.add_middleware(mw_logger)
-# app.add_middleware(BaseHTTPMiddleware, dispatch=mw_logger)
-app.middleware("http")(mw_logger)
+
+# TODO: Reimplement for robust logging
+# app.middleware("http")(mw_logger)
 
 app.include_router(health.router)
 
