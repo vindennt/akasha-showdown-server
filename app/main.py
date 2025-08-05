06b384d14c7d31be36e9c5914dd70c7ab76680a8
vindennt <truong.dennis00@gmail.com>
2025-08-05 13:46:54 -0700

    Adds db initalizing on startup

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
 from app.api.router import api_router
+from app.core.db import engine, init_db
+from sqlmodel import Session
 
 app = FastAPI()
 
@@ -19,3 +21,8 @@ app.include_router(api_router)
 @app.get("/", tags=["root"])
 async def root():
     return {"message": "\/   /\     /   O   \      /O\  "}
+
+@app.on_event("startup")
+def on_startup():
+    with Session(engine) as session:
+        init_db(session)
